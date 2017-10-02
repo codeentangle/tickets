385783ecccdcd2bacb9dd1f9f62eee1405e03492
Mathias Rottensten Kofoed <codeentangle@users.noreply.github.com>
2017-10-02 12:07:26 +0200

    Strip gnome specific stuff

--- valhalla.py
+++ valhalla.py
@@ -2,13 +2,9 @@ import re
 import webbrowser
 import urllib
 from urllib.request import urlopen
-import gi
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify
 import time
 
 url = 'http://aspx.ebillet.dk/simple/buy.listtt.aspx?act=Buy&refno=42617&orgno=156&sysno=3'
-debug_url = 'http://192.168.0.12:8080'
 itterations = 0
 
 def queryTickets():
@@ -21,9 +17,6 @@ def queryTickets():
 
 		if result:
 			print(str(itterations) + ": Tickets found!!!", end="\r")
-			Notify.init("Tickets Available")
-			Notice = Notify.Notification.new("Tickets Available", url)
-			Notice.show()
 			webbrowser.open_new(url)
 
 		else:
